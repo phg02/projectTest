36876bd7561b4dfb5b3cb50c0bfeb20cb1461584
phg02 <s4063236@rmit.edu.vn>
2024-01-22 16:30:08 +0700

    no change

--- src/main/java/helper/database.java
+++ src/main/java/helper/database.java
@@ -1,8 +1,8 @@
 package helper;
 
 public class database {
-// Name of database file (contained in database folder)
-protected static final String DATABASE = "jdbc:sqlite:database/ClimateChange.db";
+    // Name of database file (contained in database folder)
+    protected static final String DATABASE = "jdbc:sqlite:database/ClimateDB.db";
 
     public static void main(String[] args) {
         createDatabase.create();
