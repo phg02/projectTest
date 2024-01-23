3b74ce10f5adb95e7b8c9d0e001fd2e033f7e90b
NghiaPham2805 <s4028630@rmit.edu.vn>
2024-01-23 15:26:55 +0700

    Nghia state

---
+++ src/main/java/helper/State.java
@@ -0,0 +1,63 @@
+package helper;
+
+import java.io.*;
+import java.sql.*;
+import java.util.Map;
+import com.opencsv.*;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class State {
+    private static final String CSV_FILE = "database/GlobalYearlyLandTempByState.csv";
+
+    static void StateTable(){
+        try (Connection connection = DriverManager.getConnection(database.DATABASE)) {
+            String query = "INSERT INTO StateTemperature (year,AVG_temp,Min_temp,Max_temp,state_name,country_code) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader(CSV_FILE));
+            // create Map object
+            Map<String, String> line;
+            int count = 0;
+
+            while ((line = reader.readMap())!= null) {
+                for (int year = 1960; year <= 2013; year++) {
+                String year1 = line.get("Year");
+                String averageTemperature = line.get("AVG_temp");
+                String minimumTemperature = line.get("Min_temp");
+                String maximumTemperature = line.get("Max_temp");
+                String state = line.get("state_name");
+                //String country = line.get("country_code");
+                if (averageTemperature != null && !averageTemperature.isEmpty()) {
+                    statement.setDouble(2, Double.parseDouble(averageTemperature));
+                } else {
+                    statement.setObject(2, null);
+                }
+                if (minimumTemperature != null && !minimumTemperature.isEmpty()) {
+                    statement.setDouble(3, Double.parseDouble(minimumTemperature));
+                } else {
+                    statement.setObject(3, null);
+                }
+                if (maximumTemperature != null && !maximumTemperature.isEmpty()) {
+                    statement.setDouble(4, Double.parseDouble(maximumTemperature));
+                } else {
+                    statement.setObject(4, null);
+                }
+                System.out.println("Inserting " + year + " " + averageTemperature + " " + minimumTemperature + " " + maximumTemperature + " " + state + " " + "country" + " into State table...");
+                statement.setString(1, year1);
+                //statement.setString(2, averageTemperature);
+                //statement.setString(3, minimumTemperature);
+                //statement.setString(4, maximumTemperature);
+                statement.setString(5, state);
+                statement.setString(6, "WRD");
+                statement.executeUpdate();
+                ++count;
+                System.out.println(statement);
+                }
+            }
+            System.out.println("Inserted " + count);
+            connection.close();
+
+        }catch (SQLException | IOException | CsvValidationException e) {
+                e.printStackTrace();
+        }
+    }
+}
