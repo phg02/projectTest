591fa5a1668415095476d76c4aca418a40395d47
phg02 <s4063236@rmit.edu.vn>
2024-01-23 21:57:30 +0700

    f

--- src/main/java/helper/CityTemp.java
+++ src/main/java/helper/CityTemp.java
@@ -10,56 +10,55 @@ public class CityTemp {
     private static final String CSV_FILE = "database/GlobalYearlyLandTempByCity.csv";
 
     static void CityTempTable() {
-        try(Connection connection = DriverManager.getConnection(database.DATABASE)) {
-            //Create a statment to make a table
+        try (Connection connection = DriverManager.getConnection(database.DATABASE)) {
+            // Create a statment to make a table
             String query = "INSERT INTO CityTemperature (country_code, city_name, year, AVG_temp, MIN_temp, MAX_temp) VALUES(?, ?, ?, ?, ?, ?)";
-            //Name the statement
+            // Name the statement
             PreparedStatement statement = connection.prepareStatement(query);
 
-            //Create a CSV reader object
+            // Create a CSV reader object
             CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader(CSV_FILE));
-            //Create a Map object
+            // Create a Map object
             Map<String, String> line;
             long count = 0;
 
-            while((line = reader.readMap()) != null) {
+            while ((line = reader.readMap()) != null) {
                 String CountryCode = "VNM";
                 statement.setString(1, CountryCode);
                 String CityName = line.get("City");
                 statement.setString(2, CityName);
                 String year = line.get("Year");
                 statement.setInt(3, Integer.parseInt(year));
-                //Check null values
+                // Check null values
                 // If a value is a null value, then set that value to be NULL
                 // Else, set the value to it.
                 String AVGTemp = line.get("AverageTemperature");
                 if (AVGTemp != null && !AVGTemp.isEmpty()) {
                     statement.setDouble(4, Double.parseDouble(AVGTemp));
-                }
-                else {
+                } else {
                     statement.setObject(4, null);
                 }
                 String MinTemp = line.get("MinimumTemperature");
                 if (MinTemp != null && !MinTemp.isEmpty()) {
                     statement.setDouble(5, Double.parseDouble(MinTemp));
-                }
-                else {
+                } else {
                     statement.setObject(5, null);
                 }
                 String MaxTemp = line.get("MaximumTemperature");
                 if (MaxTemp != null && !MaxTemp.isEmpty()) {
                     statement.setDouble(6, Double.parseDouble(MaxTemp));
-                }
-                else {
+                } else {
                     statement.setObject(6, null);
                 }
                 statement.executeUpdate();
+                System.out.println("Inserting " + CountryCode + ", " + CityName + ", " + year + ", " + AVGTemp + ", "
+                        + MinTemp + ", " + MaxTemp);
                 count++;
 
             }
             System.out.println("Total inserts: " + count);
             connection.close();
-        }catch (SQLException | IOException | CsvValidationException e) {
+        } catch (SQLException | IOException | CsvValidationException e) {
             e.printStackTrace();
         }
     }
